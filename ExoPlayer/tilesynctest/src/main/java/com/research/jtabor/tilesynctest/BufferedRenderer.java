9b71223b33b845b418cfc0a73b8b18c90d403925
Josh <josh.r.tabor@gmail.com>
2019-03-12 14:19:07 -0500

    best try at rendering to texture..

--- ExoPlayer/tilesynctest/src/main/java/com/research/jtabor/tilesynctest/BufferedRenderer.java
+++ ExoPlayer/tilesynctest/src/main/java/com/research/jtabor/tilesynctest/BufferedRenderer.java
@@ -32,6 +32,10 @@ public class BufferedRenderer implements GLSurfaceView.Renderer {
     SurfaceTest st;
     Context context;
 
+    int frameWidth = 0;
+    int frameHeight = 0;
+
+
     private final String vertexShaderSource =
             "//VERTEX SHADER\n" +
                     "attribute vec3 vPosition;\n" +
@@ -47,7 +51,7 @@ public class BufferedRenderer implements GLSurfaceView.Renderer {
                     "#extension GL_OES_EGL_image_external : require\n" +
                     "precision mediump float;\n" +
                     "uniform vec3 vColor;\n" +
-                    "uniform sampler2D s_texture;\n" +
+                    "uniform samplerExternalOES s_texture;\n" +
                     "varying vec2 texCoordinates;\n" +
                     "void main(){\n" +
                     "\tgl_FragColor = texture2D(s_texture,texCoordinates);\n" +
@@ -201,14 +205,20 @@ static float baseTile[] =  {
         GLES20.glGenTextures(3,renderTextures,0);
         for (int i = 0; i < 3; i++){
             GLES20.glBindTexture(GLES20.GL_TEXTURE_2D,renderTextures[i]);
-            GLES20.glTexImage2D(GLES20.GL_TEXTURE_2D, 0, GLES20.GL_RGBA, 1920,1080, 0, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, null);
+            GLES20.glTexImage2D(GLES20.GL_TEXTURE_2D, 0, GLES20.GL_RGBA, 2048,2048, 0, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, null);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
         }
-        FBOTexture = createFBOTexture(1920,1080, renderTextures);
+        FBOTexture = createFBOTexture(2048,2048, renderTextures);
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl10, int width, int height) {
         //do nothing..  just a prototype.
+        frameHeight = height;
+        frameWidth = width;
         GLES20.glViewport(0,0,width,height);
         float ratio = (float) width/height;
     }
@@ -225,6 +235,7 @@ static float baseTile[] =  {
             GLES20.glActiveTexture(textureUnits[0]);
             checkErrors("2");
             GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, textureHandles[i]);
+            checkErrors("4");
             GLES20.glUniform1i(textureLocation_render, 0);
             checkErrors("5");
             GLES20.glEnableVertexAttribArray(vPosition_render);
@@ -232,11 +243,16 @@ static float baseTile[] =  {
             GLES20.glVertexAttribPointer(vPosition_render, 3, GLES20.GL_FLOAT, false, 3 * 4, vertexBuffer[i]);
             checkErrors("8");
             GLES20.glEnableVertexAttribArray(texCoord_render);
+            checkErrors("9");
             GLES20.glVertexAttribPointer(texCoord_render, 2, GLES20.GL_FLOAT, false, 2 * 4, texCoordsBuffer[i]);
-            GLES20.glBindRenderbuffer(GLES20.GL_FRAMEBUFFER,FBOTexture);
-//            GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER,0);
-            GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, allTiles[i].length / 3);
+            checkErrors("10");
+            //GLES20.glBindRenderbuffer(GLES20.GL_FRAMEBUFFER,FBOTexture);
+            GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER,FBOTexture);
+            GLES20.glViewport(0,0,2048,2048);
             checkErrors("11");
+            //GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER,0);
+            GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, allTiles[i].length / 3);
+            checkErrors("12");
         }
 
         GLES20.glUseProgram(glslProgramOneTile);
@@ -253,6 +269,7 @@ static float baseTile[] =  {
         GLES20.glVertexAttribPointer(texCoord_all, 2, GLES20.GL_FLOAT, false, 2 * 4, texCoordsBuffer[0]);
         //draw the render texture at the end.
         GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER,0);
+        GLES20.glViewport(0,0,frameWidth,frameHeight);
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, allTiles[0].length / 3);
         checkErrors("a11");
     }
@@ -268,15 +285,22 @@ static float baseTile[] =  {
         }
         return shaderHandle;
     }
+    int renderBufferActual;
     private int createFBOTexture(int width, int height, int[] renderTextures) {
         int[] temp = new int[1];
         GLES20.glGenFramebuffers(1, temp, 0);
         checkErrors("x4");
         int handleID = temp[0];
+
+        GLES20.glGenRenderbuffers(1,temp,0);
+        renderBufferActual = temp[0];
         GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, handleID);
         checkErrors("x5");
-
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D,renderTextures[0]);
         int fboTex = renderTextures[0];
+        GLES20.glBindRenderbuffer(GLES20.GL_RENDERBUFFER,renderBufferActual);
+        GLES20.glRenderbufferStorage(GLES20.GL_RENDERBUFFER,GLES20.GL_RGBA4,2048,2048);
+
         GLES20.glFramebufferTexture2D(GLES20.GL_FRAMEBUFFER, GLES20.GL_COLOR_ATTACHMENT0, GLES20.GL_TEXTURE_2D, fboTex, 0);
         checkErrors("x6");
         int status = GLES20.glCheckFramebufferStatus(GLES20.GL_FRAMEBUFFER);
@@ -284,7 +308,7 @@ static float baseTile[] =  {
             Log.e("JOSH","Framebuffer error: " + status);
             throw new IllegalStateException("GL_FRAMEBUFFER status incomplete");
         }
-        GLES20.glFramebufferRenderbuffer(GLES20.GL_FRAMEBUFFER,GLES20.GL_COLOR_ATTACHMENT0,GLES20.GL_RENDERBUFFER,0);
+        GLES20.glFramebufferRenderbuffer(GLES20.GL_FRAMEBUFFER,GLES20.GL_COLOR_ATTACHMENT0,GLES20.GL_RENDERBUFFER,renderBufferActual);
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
         GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0);
         return handleID;
